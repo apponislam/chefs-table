d6d8a89725f18b3b77e32399a48f51d2583bfb51
apponislame <11appon11@gmail.com>
2024-03-16 00:30:28 +0600

    recipe added

---
+++ src/Components/Food/Food.jsx
@@ -0,0 +1,36 @@
+import PropTypes from "prop-types";
+import { CiClock2 } from "react-icons/ci";
+import { FaFire } from "react-icons/fa";
+
+const Food = ({ food }) => {
+    // const { calories, ingredients, preparing_time, recipe_id, recipe_image, recipe_name, short_description } = food;
+    const { recipe_name, recipe_image, short_description, ingredients, preparing_time, calories } = food;
+    console.log(food);
+    return (
+        <div className="p-6 rounded-2xl border border-[#28282833]">
+            <img src={recipe_image} className="w-full mb-6 rounded-2xl" />
+            <h1 className="lexend font-semibold text-[20px] mb-4">{recipe_name}</h1>
+            <h5 className="fira mb-6 pb-4 border-b border-[#2828281A] text-[#878787]">{short_description}</h5>
+            <div className="mb-6 pb-4 border-b border-[#2828281A]">
+                <h3 className="lexend text-[18px] font-medium mb-4">Ingredients: {ingredients.length}</h3>
+            </div>
+            <div className="flex items-center mb-6">
+                <span className="text-2xl mr-2">
+                    <CiClock2 />
+                </span>
+                <span className="fira mr-4">{preparing_time}</span>
+                <span className="text-2xl mr-2">
+                    <FaFire />
+                </span>
+                <span>{calories}</span>
+            </div>
+            <button className="btn text-[18px] lexend font-medium py-3 px-6 rounded-full bg-[#0BE58A]">Want to Cook</button>
+        </div>
+    );
+};
+
+Food.propTypes = {
+    food: PropTypes.object.isRequired,
+};
+
+export default Food;
--- src/Components/mainarea/Main.jsx
+++ src/Components/mainarea/Main.jsx
@@ -1,4 +1,15 @@
+import { useEffect } from "react";
+import { useState } from "react";
+import Food from "../Food/Food";
+
 const Main = () => {
+    const [food, setFood] = useState([]);
+    useEffect(() => {
+        fetch("food.json")
+            .then((response) => response.json())
+            .then((json) => setFood(json));
+    }, []);
+
     return (
         <div className="container mx-auto">
             <div>
@@ -6,6 +17,18 @@ const Main = () => {
                 <p className="lexend text-center text-black mb-12">
                     Lorem ipsum dolor sit amet consectetur. Proin et feugiat senectus vulputate netus pharetra rhoncus. Eget <br /> urna volutpat curabitur elementum mauris aenean neque.
                 </p>
+            </div>
+            <div className="flex gap-6">
+                <div className="w-3/5">
+                    <div className="grid grid-cols-2 gap-6">
+                        {food.map((food, idx) => (
+                            <Food key={idx} food={food}></Food>
+                        ))}
+                    </div>
+                </div>
+                <div className="w-2/5">
+                    <h1>section 2</h1>
+                </div>
             </div>
         </div>
     );

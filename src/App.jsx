3bd38c71ecd2d7a6422976c04d33004db8726f1d
apponislame <11appon11@gmail.com>
2024-03-15 23:15:12 +0600

    json file added

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import "./App.css";
 import Header from "./Components/Header/Header";
 import Heroarea from "./Components/Heroarea/Heroarea";
+import Main from "./Components/mainarea/Main";
 
 function App() {
     return (
         <>
             <Header></Header>
             <Heroarea></Heroarea>
+            <Main></Main>
         </>
     );
 }
---
+++ src/Components/mainarea/Main.jsx
@@ -0,0 +1,14 @@
+const Main = () => {
+    return (
+        <div className="container mx-auto">
+            <div>
+                <h1 className="text-[40px] mb-6 text-center lexend text-black font-semibold">Our Recipes</h1>
+                <p className="lexend text-center text-black mb-12">
+                    Lorem ipsum dolor sit amet consectetur. Proin et feugiat senectus vulputate netus pharetra rhoncus. Eget <br /> urna volutpat curabitur elementum mauris aenean neque.
+                </p>
+            </div>
+        </div>
+    );
+};
+
+export default Main;
